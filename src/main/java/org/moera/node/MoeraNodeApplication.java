1f610e847923015d4f78feaa7621eb417a5ab8d1
Shmuel Melamud <smelamud@redhat.com>
2018-12-21 00:07:22 +0200

    Introducing Config.

--- src/main/java/org/moera/node/MoeraNodeApplication.java
+++ src/main/java/org/moera/node/MoeraNodeApplication.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Config.class)
 public class MoeraNodeApplication implements WebMvcConfigurer {
 
     private static Logger log = LoggerFactory.getLogger(MoeraNodeApplication.class);
--- src/main/java/org/moera/node/naming/NamingClient.java
+++ src/main/java/org/moera/node/naming/NamingClient.java
@@ -4,11 +4,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.PublicKey;
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 import com.googlecode.jsonrpc4j.ProxyUtil;
 import org.moera.commons.util.Util;
 import org.moera.naming.rpc.NamingService;
+import org.moera.node.Config;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,9 +18,12 @@ public class NamingClient {
 
     private NamingService namingService;
 
+    @Inject
+    private Config config;
+
     @PostConstruct
     protected void init() throws MalformedURLException {
-        JsonRpcHttpClient client = new JsonRpcHttpClient(new URL("http://localhost.localdomain:8081/moera-naming"));
+        JsonRpcHttpClient client = new JsonRpcHttpClient(new URL(config.getNamingServer()));
         namingService = ProxyUtil.createClientProxy(getClass().getClassLoader(), NamingService.class, client);
     }
 
