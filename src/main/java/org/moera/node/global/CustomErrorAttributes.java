35ea3ed98ca25322376a91b71e5988454394f06d
Shmuel Melamud <smelamud@redhat.com>
2018-12-29 19:18:46 +0200

    Handle "404 File not found" providing the standard JSON response.

---
+++ src/main/java/org/moera/node/global/CustomErrorAttributes.java
@@ -0,0 +1,35 @@
+package org.moera.node.global;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.inject.Inject;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Inject
+    private MessageSource messageSource;
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        Map<String, Object> ea = super.getErrorAttributes(webRequest, includeStackTrace);
+        if (Objects.equals(ea.get("status"), 404)) {
+            Map<String, Object> errorAttributes = new HashMap<>();
+            String errorCode = "not-found";
+            String message = messageSource.getMessage(errorCode, null, Locale.getDefault());
+            errorAttributes.put("errorCode", errorCode);
+            errorAttributes.put("message", message);
+            return errorAttributes;
+        }
+        return ea;
+    }
+
+}
