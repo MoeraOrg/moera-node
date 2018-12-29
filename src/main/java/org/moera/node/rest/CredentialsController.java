700defff4dd6557c95c76b7abf24878149c09fd4
Shmuel Melamud <smelamud@redhat.com>
2018-12-29 20:29:51 +0200

    Block credentials creation if they are already created.

--- src/main/java/org/moera/node/rest/CredentialsController.java
+++ src/main/java/org/moera/node/rest/CredentialsController.java
@@ -5,8 +5,10 @@ import javax.validation.Valid;
 
 import org.moera.commons.util.Password;
 import org.moera.node.model.Credentials;
+import org.moera.node.model.OperationFailure;
 import org.moera.node.model.Result;
 import org.moera.node.option.Options;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,10 @@ public class CredentialsController {
     @PostMapping
     @ResponseBody
     public Result post(@Valid @RequestBody Credentials credentials) {
+        if (!StringUtils.isEmpty(options.getString("credentials.login"))
+                && !StringUtils.isEmpty(options.getString("credentials.password-hash"))) {
+            throw new OperationFailure("credentials.already-created");
+        }
         options.set("credentials.login", credentials.getLogin());
         options.set("credentials.password-hash", Password.hash(credentials.getPassword()));
 
