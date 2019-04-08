359d4459884aaac9e1d31606499703feb50ece2c
Shmuel Melamud <smelamud@redhat.com>
2019-04-08 17:54:40 +0300

    Build titles for the pages in the node UI.

--- src/main/java/org/moera/node/ui/IndexUiController.java
+++ src/main/java/org/moera/node/ui/IndexUiController.java
@@ -10,6 +10,7 @@ import org.moera.node.model.ProfileInfo;
 import org.moera.node.model.RegisteredNameInfo;
 import org.moera.node.option.Options;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @UiController
@@ -21,6 +22,7 @@ public class IndexUiController {
     @GetMapping("/")
     @VirtualPage("/")
     private String index(Model model) {
+        model.addAttribute("pageTitle", buildPageTitle("Timeline"));
         model.addAttribute("menuIndex", "index");
 
         return "index";
@@ -29,11 +31,31 @@ public class IndexUiController {
     @GetMapping("/profile")
     @VirtualPage("/profile")
     public String profile(Model model, HttpServletResponse response) {
+        model.addAttribute("pageTitle", buildPageTitle("Profile"));
         model.addAttribute("menuIndex", "profile");
         model.addAttribute("registeredName", new RegisteredNameInfo(options, new PublicRequestContext()));
         model.addAttribute("profile", new ProfileInfo(options, new PublicRequestContext()));
 
         return "profile";
+    }
+
+    private CharSequence buildPageTitle(String title) {
+        StringBuilder buf = new StringBuilder();
+        if (!StringUtils.isEmpty(title)) {
+            buf.append(title);
+            buf.append(' ');
+            String name = options.getString("profile.registered-name");
+            if (!StringUtils.isEmpty(name)) {
+                buf.append("@ ");
+                buf.append(name);
+                buf.append(' ');
+            }
+        }
+        if (buf.length() > 0) {
+            buf.append("| ");
+        }
+        buf.append("Moera");
+        return buf;
     }
 
 }
