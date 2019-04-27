f82b8315abac9b22f079b2702aae1ee0b954ca9d
Shmuel Melamud <smelamud@redhat.com>
2019-04-28 00:31:50 +0300

    Added domains table.

---
+++ src/main/java/org/moera/node/data/Domain.java
@@ -0,0 +1,37 @@
+package org.moera.node.data;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "domains")
+public class Domain {
+
+    @Id
+    @Size(max = 63)
+    private String name;
+
+    @NotNull
+    private UUID nodeId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(UUID nodeId) {
+        this.nodeId = nodeId;
+    }
+
+}
---
+++ src/main/resources/db/migration/V5__add_domains_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE domains (
+    name character varying(63) NOT NULL PRIMARY KEY,
+    node_id uuid NOT NULL
+);
