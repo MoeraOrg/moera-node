c07804bf2a81166be19fead73593b600eea0a814
Shmuel Melamud <smelamud@redhat.com>
2019-07-08 01:13:35 +0300

    Redirect posting virtual path.

--- src/main/java/org/moera/node/ui/MoeraUiController.java
+++ src/main/java/org/moera/node/ui/MoeraUiController.java
@@ -3,6 +3,7 @@ package org.moera.node.ui;
 import org.moera.node.global.UiController;
 import org.moera.node.global.VirtualPage;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,6 +21,12 @@ public class MoeraUiController {
     @VirtualPage
     public String timeline(@RequestParam(required = false) Long before) {
         return before == null ? "redirect:/timeline" : String.format("redirect:/timeline?before=%d", before);
+    }
+
+    @GetMapping("/post/{id}")
+    @VirtualPage
+    public String post(@PathVariable String id) {
+        return "redirect:/post/" + id;
     }
 
     @GetMapping("/profile")
