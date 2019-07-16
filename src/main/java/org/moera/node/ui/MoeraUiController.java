3593ffac044432e74ff9a9d5b33b5a72f2c059ec
Shmuel Melamud <smelamud@redhat.com>
2019-07-17 02:20:57 +0300

    Virtual page for post composer.

--- src/main/java/org/moera/node/ui/MoeraUiController.java
+++ src/main/java/org/moera/node/ui/MoeraUiController.java
@@ -1,5 +1,6 @@
 package org.moera.node.ui;
 
+import org.moera.node.global.PageNotFoundException;
 import org.moera.node.global.UiController;
 import org.moera.node.global.VirtualPage;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +28,12 @@ public class MoeraUiController {
     @VirtualPage
     public String post(@PathVariable String id) {
         return "redirect:/post/" + id;
+    }
+
+    @GetMapping("/compose")
+    @VirtualPage
+    public String compose() {
+        throw new PageNotFoundException();
     }
 
     @GetMapping("/profile")
