0e6bba7db474cfb71c1998c9edf88e7170f9d659
Shmuel Melamud <smelamud@redhat.com>
2019-10-13 00:23:39 +0300

    Checking modifiers for Duration type.

--- src/main/java/org/moera/node/option/type/DurationOptionType.java
+++ src/main/java/org/moera/node/option/type/DurationOptionType.java
@@ -4,10 +4,36 @@ import java.time.Duration;
 
 import org.moera.commons.util.DurationFormatException;
 import org.moera.commons.util.Util;
+import org.moera.node.option.OptionTypeModifiers;
+import org.moera.node.option.exception.DeserializeOptionValueException;
 import org.moera.node.option.exception.UnsuitableOptionValueException;
 
 @OptionType("Duration")
 public class DurationOptionType extends OptionTypeBase {
+
+    private long parseSeconds(String value) {
+        try {
+            return Util.toDuration(value).getSeconds();
+        } catch (DurationFormatException e) {
+            throw new DeserializeOptionValueException(getTypeName(), value);
+        }
+    }
+
+    @Override
+    public DurationOptionTypeModifiers parseTypeModifiers(OptionTypeModifiers modifiers) {
+        DurationOptionTypeModifiers durMods = new DurationOptionTypeModifiers();
+        if (modifiers != null && modifiers.getMin() != null) {
+            durMods.setMinSeconds(parseSeconds(modifiers.getMin()));
+        } else {
+            durMods.setMinSeconds(0);
+        }
+        if (modifiers != null && modifiers.getMax() != null) {
+            durMods.setMaxSeconds(parseSeconds(modifiers.getMax()));
+        } else {
+            durMods.setMaxSeconds(Long.MAX_VALUE);
+        }
+        return durMods;
+    }
 
     @Override
     public String getString(Object value) {
@@ -20,16 +46,26 @@ public class DurationOptionType extends OptionTypeBase {
     }
 
     @Override
-    protected Object accept(Object value) {
+    public Object accept(Object value, Object typeModifiers) {
         if (value instanceof String) {
-            try {
-                Util.toDuration((String) value);
-                return value;
-            } catch (DurationFormatException e) {
-                throw new UnsuitableOptionValueException((String) value);
-            }
+            return acceptString((String) value, (DurationOptionTypeModifiers) typeModifiers);
         }
         return super.accept(value);
+    }
+
+    private String acceptString(String value, DurationOptionTypeModifiers typeModifiers) {
+        long seconds;
+        try {
+            seconds = Util.toDuration(value).getSeconds();
+        } catch (DurationFormatException e) {
+            throw new UnsuitableOptionValueException(value);
+        }
+        if (typeModifiers != null) {
+            if (seconds < typeModifiers.getMinSeconds() || seconds > typeModifiers.getMaxSeconds()) {
+                throw new UnsuitableOptionValueException(value);
+            }
+        }
+        return value;
     }
 
 }
---
+++ src/main/java/org/moera/node/option/type/DurationOptionTypeModifiers.java
@@ -0,0 +1,24 @@
+package org.moera.node.option.type;
+
+public class DurationOptionTypeModifiers {
+
+    private long minSeconds;
+    private long maxSeconds;
+
+    public long getMinSeconds() {
+        return minSeconds;
+    }
+
+    public void setMinSeconds(long minSeconds) {
+        this.minSeconds = minSeconds;
+    }
+
+    public long getMaxSeconds() {
+        return maxSeconds;
+    }
+
+    public void setMaxSeconds(long maxSeconds) {
+        this.maxSeconds = maxSeconds;
+    }
+
+}
