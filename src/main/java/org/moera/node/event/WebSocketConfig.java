2a2f53465d641cc158099b0aae5064a1122c5a76
Shmuel Melamud <smelamud@redhat.com>
2019-11-10 00:26:45 +0200

    Events WebSockets endpoint moved to /moera/api/events.

--- src/main/java/org/moera/node/event/WebSocketConfig.java
+++ src/main/java/org/moera/node/event/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/moera/events")
+        registry.addEndpoint("/moera/api/events")
             .setAllowedOrigins("*");
     }
 
