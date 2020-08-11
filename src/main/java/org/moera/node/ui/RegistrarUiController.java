ac07714032c3217b1e3e2b44939ddd4ad678fb01
Shmuel Melamud <smelamud@redhat.com>
2020-08-11 21:28:36 +0300

    Registration form completed.

--- src/main/java/org/moera/node/ui/RegistrarUiController.java
+++ src/main/java/org/moera/node/ui/RegistrarUiController.java
@@ -1,14 +1,20 @@
 package org.moera.node.ui;
 
 import org.moera.node.global.UiController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @UiController
 public class RegistrarUiController {
 
+    @Value("${registrar.domain}")
+    private String registrarDomain;
+
     @GetMapping("/registrar")
     private String index(Model model) {
+        model.addAttribute("registrarDomain", registrarDomain);
+
         return "registrar/index";
     }
 
