c52d982fe566b849a3bf1105ef6a16109a5f18ec
Shmuel Melamud <smelamud@redhat.com>
2020-08-12 01:57:10 +0300

    Registration process finished.

---
+++ src/main/java/org/moera/node/registrar/RegistrarHost.java
@@ -0,0 +1,15 @@
+package org.moera.node.registrar;
+
+public class RegistrarHost {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+}
