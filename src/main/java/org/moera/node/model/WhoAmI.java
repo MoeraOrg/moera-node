a9a15798b33a2f612e10ddbd2e216c42dbe7f924
Shmuel Melamud <smelamud@redhat.com>
2020-12-06 21:09:41 +0200

    Report node name changing status from /whoami.

--- src/main/java/org/moera/node/model/WhoAmI.java
+++ src/main/java/org/moera/node/model/WhoAmI.java
@@ -1,18 +1,24 @@
 package org.moera.node.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.moera.naming.rpc.OperationStatus;
 import org.moera.node.option.Options;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WhoAmI {
 
     private String nodeName;
+    private boolean nodeNameChanging;
 
     public WhoAmI() {
     }
 
     public WhoAmI(Options options) {
         nodeName = options.nodeName();
+        OperationStatus status = OperationStatus.forValue(options.getString("naming.operation.status"));
+        nodeNameChanging = status == OperationStatus.WAITING
+                || status == OperationStatus.ADDED
+                || status == OperationStatus.STARTED;
     }
 
     public String getNodeName() {
@@ -21,6 +27,14 @@ public class WhoAmI {
 
     public void setNodeName(String nodeName) {
         this.nodeName = nodeName;
+    }
+
+    public boolean isNodeNameChanging() {
+        return nodeNameChanging;
+    }
+
+    public void setNodeNameChanging(boolean nodeNameChanging) {
+        this.nodeNameChanging = nodeNameChanging;
     }
 
 }
