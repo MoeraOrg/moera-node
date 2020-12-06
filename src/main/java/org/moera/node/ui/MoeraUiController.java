5b95e797ccd8788843a740728439de33b06332d3
Shmuel Melamud <smelamud@redhat.com>
2020-12-06 21:56:18 +0200

    For Moera pages that do not exist in Web UI, display a stub page with a button to switch to Web Client.

--- src/main/java/org/moera/node/ui/MoeraUiController.java
+++ src/main/java/org/moera/node/ui/MoeraUiController.java
@@ -1,8 +1,12 @@
 package org.moera.node.ui;
 
-import org.moera.node.global.PageNotFoundException;
+import javax.inject.Inject;
+
+import org.moera.node.global.RequestContext;
 import org.moera.node.global.UiController;
 import org.moera.node.global.VirtualPage;
+import org.moera.node.util.Util;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @UiController
 @RequestMapping("/moera")
 public class MoeraUiController {
+
+    @Inject
+    private RequestContext requestContext;
+
+    @Inject
+    private TitleBuilder titleBuilder;
 
     @GetMapping
     @VirtualPage
@@ -32,8 +42,8 @@ public class MoeraUiController {
 
     @GetMapping("/compose")
     @VirtualPage
-    public String compose() {
-        throw new PageNotFoundException();
+    public String compose(Model model) {
+        return openClient("New Post", model);
     }
 
     @GetMapping("/profile")
@@ -44,20 +54,20 @@ public class MoeraUiController {
 
     @GetMapping("/settings")
     @VirtualPage
-    public String settings() {
-        throw new PageNotFoundException();
+    public String settings(Model model) {
+        return openClient("Settings", model);
     }
 
     @GetMapping("/settings/{tab}")
     @VirtualPage
-    public String settingsTab(@PathVariable String tab) {
-        throw new PageNotFoundException();
+    public String settingsTab(@PathVariable String tab, Model model) {
+        return openClient("Settings", model);
     }
 
     @GetMapping("/news")
     @VirtualPage
-    public String news() {
-        throw new PageNotFoundException();
+    public String news(Model model) {
+        return openClient("News", model);
     }
 
     @GetMapping("/people")
@@ -70,6 +80,13 @@ public class MoeraUiController {
     @VirtualPage
     public String peopleTab(@PathVariable String tab) {
         return "redirect:/people/" + tab;
+    }
+
+    private String openClient(String title, Model model) {
+        model.addAttribute("pageTitle", titleBuilder.build(title));
+        model.addAttribute("url", Util.ue(requestContext.getUrl()));
+
+        return "client";
     }
 
 }
