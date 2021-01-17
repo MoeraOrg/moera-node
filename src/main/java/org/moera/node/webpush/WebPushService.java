ddb9cf452f1d0b9711576f3f21b58fca0f8ce665
Shmuel Melamud <smelamud@redhat.com>
2021-01-17 20:18:14 +0200

    Unused code.

--- src/main/java/org/moera/node/webpush/WebPushService.java
+++ src/main/java/org/moera/node/webpush/WebPushService.java
@@ -1,7 +1,6 @@
 package org.moera.node.webpush;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -19,14 +18,12 @@ import javax.inject.Inject;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.novacrypto.bip39.Words;
 import nl.martijndwars.webpush.Notification;
 import nl.martijndwars.webpush.PushService;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.spec.ECParameterSpec;
-import org.bouncycastle.math.ec.ECPoint;
 import org.jose4j.lang.JoseException;
 import org.moera.commons.crypto.CryptoException;
 import org.moera.node.data.WebPushSubscription;
@@ -65,16 +62,9 @@ public class WebPushService {
     public PublicKey generateKeys(Options options) {
         try {
             ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec(WebPush.EC_CURVE);
-
-            SecureRandom random = new SecureRandom();
-            byte[] seed = new byte[Words.TWENTY_FOUR.byteLength()];
-            random.nextBytes(seed);
-
-            BigInteger d = new BigInteger(seed);
-            ECPoint q = ecSpec.getG().multiply(d);
-
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC", "BC");
-            keyPairGenerator.initialize(ecSpec, random);
+            keyPairGenerator.initialize(ecSpec, new SecureRandom());
+
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
             options.set("web-push.private-key", keyPair.getPrivate());
             options.set("web-push.public-key", keyPair.getPublic());
