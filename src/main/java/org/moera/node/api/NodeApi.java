637be76f03afc913df68c7e8bf8e9f8680e60a53
Shmuel Melamud <smelamud@redhat.com>
2021-02-08 23:38:23 +0200

    In Node API call use Authorization: header instead of token/carte passed in parameters.

--- src/main/java/org/moera/node/api/NodeApi.java
+++ src/main/java/org/moera/node/api/NodeApi.java
@@ -68,25 +68,28 @@ public class NodeApi {
         return details != null ? UriUtil.normalize(details.getNodeUri()) : null;
     }
 
-    private <T> T call(String method, String remoteNodeName, String location, Class<T> result)
+    private <T> T call(String method, String remoteNodeName, String location, String auth, Class<T> result)
             throws NodeApiException {
 
-        return call(method, remoteNodeName, location, null, result);
+        return call(method, remoteNodeName, location, auth, null, result);
     }
 
-    private <T> T call(String method, String remoteNodeName, String location, Object body, Class<T> result)
+    private <T> T call(String method, String remoteNodeName, String location, String auth, Object body, Class<T> result)
             throws NodeApiException {
 
         String nodeUri = fetchNodeUri(remoteNodeName);
         if (nodeUri == null) {
             throw new NodeApiUnknownNameException(remoteNodeName);
         }
-        HttpRequest request = HttpRequest.newBuilder()
+        var requestBuilder = HttpRequest.newBuilder()
                 .uri(URI.create(nodeUri + "/api" + location))
                 .timeout(CALL_API_REQUEST_TIMEOUT)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .method(method, jsonPublisher(body))
-                .build();
+                .method(method, jsonPublisher(body));
+        if (auth != null) {
+            requestBuilder = requestBuilder.header(HttpHeaders.AUTHORIZATION, auth);
+        }
+        HttpRequest request = requestBuilder.build();
         HttpClient client = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.NORMAL)
                 .connectTimeout(CALL_API_CONNECTION_TIMEOUT)
@@ -121,6 +124,13 @@ public class NodeApi {
         return jsonParse(response.body(), result);
     }
 
+    private String auth(String type, String token) {
+        if (token == null) {
+            return null;
+        }
+        return type + ":" + token;
+    }
+
     private HttpRequest.BodyPublisher jsonPublisher(Object body) throws NodeApiException {
         if (body == null) {
             return HttpRequest.BodyPublishers.ofString("{}");
@@ -141,66 +151,68 @@ public class NodeApi {
     }
 
     public WhoAmI whoAmI(String nodeName) throws NodeApiException {
-        return call("GET", nodeName, "/whoami", WhoAmI.class);
+        return call("GET", nodeName, "/whoami", null, WhoAmI.class);
     }
 
     public PostingInfo getPosting(String nodeName, String postingId) throws NodeApiException {
-        return call("GET", nodeName, String.format("/postings/%s", Util.ue(postingId)), PostingInfo.class);
+        return call("GET", nodeName, String.format("/postings/%s", Util.ue(postingId)), null,
+                PostingInfo.class);
     }
 
     public PostingRevisionInfo getPostingRevision(String nodeName, String postingId, String revisionId)
             throws NodeApiException {
 
         return call("GET", nodeName,
-                String.format("/postings/%s/revisions/%s", Util.ue(postingId), Util.ue(revisionId)),
+                String.format("/postings/%s/revisions/%s", Util.ue(postingId), Util.ue(revisionId)), null,
                 PostingRevisionInfo.class);
     }
 
     public PostingRevisionInfo[] getPostingRevisions(String nodeName, String postingId) throws NodeApiException {
-        return call("GET", nodeName, String.format("/postings/%s/revisions", Util.ue(postingId)),
+        return call("GET", nodeName, String.format("/postings/%s/revisions", Util.ue(postingId)), null,
                 PostingRevisionInfo[].class);
     }
 
     public ReactionCreated postPostingReaction(String nodeName, String postingId,
                                                ReactionDescription reactionDescription) throws NodeApiException {
-        return call("POST", nodeName, String.format("/postings/%s/reactions", Util.ue(postingId)), reactionDescription,
-                ReactionCreated.class);
+        return call("POST", nodeName, String.format("/postings/%s/reactions", Util.ue(postingId)), null,
+                reactionDescription, ReactionCreated.class);
     }
 
     public ReactionInfo getPostingReaction(String nodeName, String postingId, String reactionOwnerName)
             throws NodeApiException {
 
         return call("GET", nodeName,
-                String.format("/postings/%s/reactions/%s", Util.ue(postingId), Util.ue(reactionOwnerName)),
+                String.format("/postings/%s/reactions/%s", Util.ue(postingId), Util.ue(reactionOwnerName)), null,
                 ReactionInfo.class);
     }
 
     public ReactionCreated postCommentReaction(String nodeName, String postingId, String commentId,
                                                ReactionDescription reactionDescription) throws NodeApiException {
         return call("POST", nodeName,
-                String.format("/postings/%s/comments/%s/reactions", Util.ue(postingId), Util.ue(commentId)),
+                String.format("/postings/%s/comments/%s/reactions", Util.ue(postingId), Util.ue(commentId)), null,
                 reactionDescription, ReactionCreated.class);
     }
 
     public Result postNotification(String nodeName, NotificationPacket notificationPacket) throws NodeApiException {
-        return call("POST", nodeName, "/notifications", notificationPacket, Result.class);
+        return call("POST", nodeName, "/notifications", null, notificationPacket, Result.class);
     }
 
     public SubscriberInfo postSubscriber(String nodeName, String carte, SubscriberDescriptionQ subscriber)
             throws NodeApiException {
 
-        return call("POST", nodeName, String.format("/people/subscribers?carte=%s", Util.ue(carte)), subscriber,
+        return call("POST", nodeName, "/people/subscribers", auth("carte", carte), subscriber,
                 SubscriberInfo.class);
     }
 
     public FeedSliceInfo getFeedStories(String nodeName, String feedName, int limit) throws NodeApiException {
         return call("GET", nodeName,
-                String.format("/feeds/%s/stories?limit=%d", Util.ue(feedName), limit),
+                String.format("/feeds/%s/stories?limit=%d", Util.ue(feedName), limit), null,
                 FeedSliceInfo.class);
     }
 
     public CommentInfo getComment(String nodeName, String postingId, String commentId) throws NodeApiException {
-        return call("GET", nodeName, String.format("/postings/%s/comments/%s", Util.ue(postingId), Util.ue(commentId)),
+        return call("GET", nodeName,
+                String.format("/postings/%s/comments/%s", Util.ue(postingId), Util.ue(commentId)), null,
                 CommentInfo.class);
     }
 
@@ -208,22 +220,22 @@ public class NodeApi {
                                                   String revisionId) throws NodeApiException {
         return call("GET", nodeName,
                 String.format("/postings/%s/comments/%s/revisions/%s",
-                        Util.ue(postingId), Util.ue(commentId), Util.ue(revisionId)),
+                        Util.ue(postingId), Util.ue(commentId), Util.ue(revisionId)), null,
                 CommentRevisionInfo.class);
     }
 
     public CommentCreated postComment(String nodeName, String postingId, CommentText commentText)
             throws NodeApiException {
 
-        return call("POST", nodeName, String.format("/postings/%s/comments", Util.ue(postingId)), commentText,
-                CommentCreated.class);
+        return call("POST", nodeName, String.format("/postings/%s/comments", Util.ue(postingId)), null,
+                commentText, CommentCreated.class);
     }
 
     public CommentInfo putComment(String nodeName, String postingId, String commentId, CommentText commentText)
             throws NodeApiException {
 
         return call("PUT", nodeName,
-                String.format("/postings/%s/comments/%s", Util.ue(postingId), Util.ue(commentId)),
+                String.format("/postings/%s/comments/%s", Util.ue(postingId), Util.ue(commentId)), null,
                 commentText, CommentInfo.class);
     }
 
@@ -231,7 +243,7 @@ public class NodeApi {
                                            String reactionOwnerName) throws NodeApiException {
         return call("GET", nodeName,
                 String.format("/postings/%s/comments/%s/reactions/%s",
-                        Util.ue(postingId), Util.ue(commentId), Util.ue(reactionOwnerName)),
+                        Util.ue(postingId), Util.ue(commentId), Util.ue(reactionOwnerName)), null,
                 ReactionInfo.class);
     }
 
