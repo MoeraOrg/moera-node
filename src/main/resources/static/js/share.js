123506bd1ed99144a4aea156271724e0e7fe70e3
Shmuel Melamud <smelamud@redhat.com>
2021-03-23 02:37:25 +0200

    Use Web Share API, if available.

--- src/main/resources/static/js/share.js
+++ src/main/resources/static/js/share.js
@@ -1,17 +1,26 @@
 function shareInit() {
     $(".share-button").each(function() {
         const popup = $(this).next(".share-popup");
-        const popper = Popper.createPopper(this, popup.get(0), {"placement": "top" });
-        $(this).click(function() {
-            if (popup.hasClass("show")) {
-                popup.removeClass("show");
-                popper.setOptions({modifiers: [{ name: 'eventListeners', enabled: false }]});
-            } else {
-                popup.addClass("show");
-                popper.setOptions({modifiers: [{ name: 'eventListeners', enabled: true }]});
-                popper.update();
-            }
-        })
+        if (navigator.share) {
+            const popupBody = popup.children(".shareon");
+            const title = popupBody.attr("data-title");
+            const url = popupBody.attr("data-url");
+            $(this).click(function() {
+                navigator.share({ title, url });
+            });
+        } else {
+            const popper = Popper.createPopper(this, popup.get(0), {"placement": "top" });
+            $(this).click(function() {
+                if (popup.hasClass("show")) {
+                    popup.removeClass("show");
+                    popper.setOptions({modifiers: [{ name: 'eventListeners', enabled: false }]});
+                } else {
+                    popup.addClass("show");
+                    popper.setOptions({modifiers: [{ name: 'eventListeners', enabled: true }]});
+                    popper.update();
+                }
+            });
+        }
     });
 }
 
