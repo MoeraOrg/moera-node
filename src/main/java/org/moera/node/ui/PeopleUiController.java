e4deb506ae82370344db1f39610684de2edb081a
Shmuel Melamud <smelamud@redhat.com>
2021-05-26 01:52:01 +0300

    Avatars and sorting people on Web UI People page.

--- src/main/java/org/moera/node/ui/PeopleUiController.java
+++ src/main/java/org/moera/node/ui/PeopleUiController.java
@@ -1,6 +1,8 @@
 package org.moera.node.ui;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 
 import org.moera.node.data.Subscriber;
@@ -11,6 +13,9 @@ import org.moera.node.data.SubscriptionType;
 import org.moera.node.global.RequestContext;
 import org.moera.node.global.UiController;
 import org.moera.node.global.VirtualPage;
+import org.moera.node.model.SubscriberInfo;
+import org.moera.node.model.SubscriptionInfo;
+import org.moera.node.naming.NodeName;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -39,8 +44,13 @@ public class PeopleUiController {
     public String subscribers(Model model) {
         int subscribersTotal = subscriberRepository.countAllByType(requestContext.nodeId(), SubscriptionType.FEED);
         int subscriptionsTotal = subscriptionRepository.countByType(requestContext.nodeId(), SubscriptionType.FEED);
-        List<Subscriber> subscribers = subscriberRepository.findAllByType(requestContext.nodeId(),
-                SubscriptionType.FEED);
+        Comparator<Subscriber> comparator = Comparator.comparing(
+                sr -> sr.getRemoteFullName() != null ? sr.getRemoteFullName() : NodeName.shorten(sr.getRemoteNodeName()));
+        List<SubscriberInfo> subscribers =
+                subscriberRepository.findAllByType(requestContext.nodeId(), SubscriptionType.FEED).stream()
+                        .sorted(comparator)
+                        .map(SubscriberInfo::new)
+                        .collect(Collectors.toList());
 
         model.addAttribute("pageTitle", titleBuilder.build("Subscribers"));
         model.addAttribute("menuIndex", "people");
@@ -56,8 +66,13 @@ public class PeopleUiController {
     public String subscriptions(Model model) {
         int subscribersTotal = subscriberRepository.countAllByType(requestContext.nodeId(), SubscriptionType.FEED);
         int subscriptionsTotal = subscriptionRepository.countByType(requestContext.nodeId(), SubscriptionType.FEED);
-        List<Subscription> subscriptions = subscriptionRepository.findAllByType(requestContext.nodeId(),
-                SubscriptionType.FEED);
+        Comparator<Subscription> comparator = Comparator.comparing(
+                sr -> sr.getRemoteFullName() != null ? sr.getRemoteFullName() : NodeName.shorten(sr.getRemoteNodeName()));
+        List<SubscriptionInfo> subscriptions =
+                subscriptionRepository.findAllByType(requestContext.nodeId(), SubscriptionType.FEED).stream()
+                        .sorted(comparator)
+                        .map(SubscriptionInfo::new)
+                        .collect(Collectors.toList());
 
         model.addAttribute("pageTitle", titleBuilder.build("Subscriptions"));
         model.addAttribute("menuIndex", "people");
