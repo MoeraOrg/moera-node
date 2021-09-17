fcea30dded7035fd146c12fb776edf6956b7baa7
Shmuel Melamud <smelamud@redhat.com>
2021-09-17 05:11:34 +0300

    Do not reveal text under spoiler when creating a heading.

--- src/main/java/org/moera/node/text/HeadingExtractor.java
+++ src/main/java/org/moera/node/text/HeadingExtractor.java
@@ -2,6 +2,7 @@ package org.moera.node.text;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
 import org.jsoup.select.NodeFilter;
@@ -29,7 +30,8 @@ public class HeadingExtractor {
 
     private static class Extractor implements NodeFilter {
 
-        private StringBuilder result = new StringBuilder();
+        private final StringBuilder result = new StringBuilder();
+        private boolean ignoreContent = false;
 
         String getResult() {
             return result.toString();
@@ -37,20 +39,37 @@ public class HeadingExtractor {
 
         @Override
         public FilterResult head(Node node, int depth) {
-            if (node instanceof TextNode) {
-                String text = ((TextNode) node).text().trim();
-                if (!text.isEmpty()) {
-                    if (result.length() > 0) {
-                        result.append(' ');
+            if (!ignoreContent) {
+                String text = null;
+                if (node instanceof TextNode) {
+                    text = clear(((TextNode) node).text().trim());
+                } else if (node instanceof Element) {
+                    Element element = (Element) node;
+                    if (element.normalName().equals("mr-spoiler")) {
+                        ignoreContent = true;
+                        text = element.hasAttr("title") ? element.attr("title") : "spoiler!";
+                        text = "[" + text + "]";
                     }
-                    result.append(clear(text));
-                    if (result.length() >= HEADING_LENGTH) {
-                        Util.ellipsize(result, HEADING_LENGTH);
-                        return FilterResult.STOP;
-                    }
+                }
+                if (appendText(text)) {
+                    return FilterResult.STOP;
                 }
             }
             return FilterResult.CONTINUE;
+        }
+
+        private boolean appendText(String text) {
+            if (!ObjectUtils.isEmpty(text)) {
+                if (result.length() > 0) {
+                    result.append(' ');
+                }
+                result.append(text);
+                if (result.length() >= HEADING_LENGTH) {
+                    Util.ellipsize(result, HEADING_LENGTH);
+                    return true;
+                }
+            }
+            return false;
         }
 
         private String clear(String text) {
@@ -59,6 +78,12 @@ public class HeadingExtractor {
 
         @Override
         public FilterResult tail(Node node, int depth) {
+            if (node instanceof Element) {
+                Element element = (Element) node;
+                if (element.normalName().equals("mr-spoiler")) {
+                    ignoreContent = false;
+                }
+            }
             return FilterResult.CONTINUE;
         }
 
