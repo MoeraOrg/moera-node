7ec43ec22fdb30613ec776e7b27abfd6528d7715
Shmuel Melamud <smelamud@redhat.com>
2021-11-15 01:20:36 +0200

    API for getting posting media limits from the backend.

--- src/main/java/org/moera/node/model/PostingFeatures.java
+++ src/main/java/org/moera/node/model/PostingFeatures.java
@@ -1,6 +1,7 @@
 package org.moera.node.model;
 
 import java.util.List;
+import javax.imageio.ImageIO;
 
 import org.moera.node.data.SourceFormat;
 import org.moera.node.option.Options;
@@ -8,13 +9,19 @@ import org.moera.node.option.Options;
 public class PostingFeatures {
 
     private boolean subjectPresent;
-    private List<Choice> sourceFormats = Choice.forEnum(SourceFormat.class);
+    private final List<Choice> sourceFormats = Choice.forEnum(SourceFormat.class);
+    private long mediaMaxSize;
+    private long imageRecommendedSize;
+    private int imageRecommendedPixels;
 
     public PostingFeatures() {
     }
 
     public PostingFeatures(Options options) {
         subjectPresent = options.getBool("posting.subject.present");
+        mediaMaxSize = options.getLong("posting.media.max-size");
+        imageRecommendedSize = options.getLong("posting.image.recommended-size");
+        imageRecommendedPixels = options.getInt("posting.image.recommended-pixels");
     }
 
     public boolean isSubjectPresent() {
@@ -27,6 +34,30 @@ public class PostingFeatures {
 
     public List<Choice> getSourceFormats() {
         return sourceFormats;
+    }
+
+    public long getMediaMaxSize() {
+        return mediaMaxSize;
+    }
+
+    public long getImageRecommendedSize() {
+        return imageRecommendedSize;
+    }
+
+    public void setImageRecommendedSize(long imageRecommendedSize) {
+        this.imageRecommendedSize = imageRecommendedSize;
+    }
+
+    public int getImageRecommendedPixels() {
+        return imageRecommendedPixels;
+    }
+
+    public void setImageRecommendedPixels(int imageRecommendedPixels) {
+        this.imageRecommendedPixels = imageRecommendedPixels;
+    }
+
+    public String[] getImageFormats() {
+        return ImageIO.getReaderMIMETypes();
     }
 
 }
