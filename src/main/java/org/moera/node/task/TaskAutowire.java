e055467ae98ccfd09d82472460e818cc83c313ff
Shmuel Melamud <smelamud@redhat.com>
2022-02-17 01:27:19 +0200

    The configured local IP address should take priority over a detected one.

--- src/main/java/org/moera/node/task/TaskAutowire.java
+++ src/main/java/org/moera/node/task/TaskAutowire.java
@@ -6,14 +6,19 @@ import java.net.UnknownHostException;
 import java.util.UUID;
 import javax.inject.Inject;
 
+import org.moera.commons.util.LogUtil;
 import org.moera.node.config.Config;
 import org.moera.node.domain.Domains;
 import org.moera.node.global.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskAutowire {
+
+    private static final Logger log = LoggerFactory.getLogger(TaskAutowire.class);
 
     @Inject
     private Config config;
@@ -30,7 +35,7 @@ public class TaskAutowire {
     public void autowire(Task task) {
         autowireCapableBeanFactory.autowireBean(task);
         task.setNodeId(requestContext.nodeId());
-        task.setLocalAddr(requestContext.getLocalAddr());
+        task.setLocalAddr(getLocalAddr());
     }
 
     public void autowireWithoutRequest(Task task, UUID nodeId) {
@@ -39,16 +44,35 @@ public class TaskAutowire {
         task.setLocalAddr(getLocalAddr(domains.getDomainName(nodeId)));
     }
 
+    private InetAddress getLocalAddr() {
+        if (config.getAddress() != null) {
+            try {
+                return InetAddress.getByName(config.getAddress());
+            } catch (UnknownHostException e) {
+                log.error("Configured IP address {} is invalid", LogUtil.format(config.getAddress()));
+            }
+        }
+
+        return requestContext.getLocalAddr();
+    }
+
     private InetAddress getLocalAddr(String domainName) {
-        domainName = domainName != null && !domainName.equals(Domains.DEFAULT_DOMAIN) ? domainName : config.getAddress();
-        if (domainName != null) {
+        if (config.getAddress() != null) {
+            try {
+                return InetAddress.getByName(config.getAddress());
+            } catch (UnknownHostException e) {
+                log.error("Configured IP address {} is invalid", LogUtil.format(config.getAddress()));
+            }
+        }
+
+        if (domainName != null && !domainName.equals(Domains.DEFAULT_DOMAIN)) {
             try {
                 InetAddress[] ips = InetAddress.getAllByName(domainName);
                 if (ips != null && ips.length > 0) {
                     return ips[0];
                 }
             } catch (UnknownHostException e) {
-                // ignore
+                log.error("Could not resolve our domain {}", LogUtil.format(domainName));
             }
         }
 
