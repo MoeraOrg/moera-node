33e0f60d931d03d6598e78cf7f1795f06cb0d454
Shmuel Melamud <smelamud@redhat.com>
2022-04-15 02:43:43 +0300

    Wording.

--- src/main/java/org/moera/node/auth/UnresolvedPrincipalException.java
+++ src/main/java/org/moera/node/auth/UnresolvedPrincipalException.java
@@ -3,7 +3,7 @@ package org.moera.node.auth;
 public class UnresolvedPrincipalException extends RuntimeException {
 
     public UnresolvedPrincipalException(Principal principal) {
-        super("Unresolved or unknown principal cannot be verified: " + principal);
+        super("Relative or unknown principal cannot be verified: " + principal);
     }
 
 }
