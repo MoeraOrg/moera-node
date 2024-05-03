4dd24a7c6782c1ced859a603d1414c8526e50aab
Shmuel Melamud <smelamud@redhat.com>
2024-05-03 05:16:50 +0300

    Rewrite SitemapRecordRepository.findUpdated() query, because native query throws a JDBC type mapping error.

--- src/main/java/org/moera/node/data/SitemapRecordRepository.java
+++ src/main/java/org/moera/node/data/SitemapRecordRepository.java
@@ -8,12 +8,10 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface SitemapRecordRepository extends JpaRepository<SitemapRecord, UUID> {
 
-    @Query(value = "select p.* from stories s"
-            + " left join entries p on s.entry_id=p.id"
-            + " left join sitemap_records r on p.id=r.entry_id"
-            + " where p.node_id=?1 and s.feed_name = 'timeline' and p is not null"
-            + " and (r is null or p.edited_at > r.modified_at) and p.deleted_at is null",
-            nativeQuery = true)
+    @Query("select p from Posting p"
+            + " where p.nodeId = ?1"
+            + " and exists(select s from Story s where s.entry.id = p.id and s.feedName = 'timeline')"
+            + " and not exists(select r from SitemapRecord r where r.entry.id = p.id and r.modifiedAt >= p.editedAt)")
     Collection<Posting> findUpdated(UUID nodeId);
 
     @Query("select r from SitemapRecord r where r.nodeId = ?1 and r.entry.id = ?2")
