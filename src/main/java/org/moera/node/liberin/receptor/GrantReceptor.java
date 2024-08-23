42bd7e4dbb128344ac76f316e8605424ce980a79
Shmuel Melamud <smelamud@redhat.com>
2024-08-23 04:17:38 +0300

    Event about updating a grant.

--- src/main/java/org/moera/node/liberin/receptor/GrantReceptor.java
+++ src/main/java/org/moera/node/liberin/receptor/GrantReceptor.java
@@ -4,6 +4,8 @@ import org.moera.node.liberin.LiberinMapping;
 import org.moera.node.liberin.LiberinReceptor;
 import org.moera.node.liberin.LiberinReceptorBase;
 import org.moera.node.liberin.model.GrantUpdatedLiberin;
+import org.moera.node.model.GrantInfo;
+import org.moera.node.model.event.GrantUpdatedEvent;
 import org.moera.node.model.notification.GrantUpdatedNotification;
 import org.moera.node.notification.send.Directions;
 
@@ -12,6 +14,7 @@ public class GrantReceptor extends LiberinReceptorBase {
 
     @LiberinMapping
     public void grantUpdated(GrantUpdatedLiberin liberin) {
+        send(liberin, new GrantUpdatedEvent(new GrantInfo(liberin.getNodeName(), liberin.getScope())));
         send(Directions.single(liberin.getNodeId(), liberin.getNodeName()),
                 new GrantUpdatedNotification(liberin.getScope()));
     }
---
+++ src/main/java/org/moera/node/model/event/GrantUpdatedEvent.java
@@ -0,0 +1,35 @@
+package org.moera.node.model.event;
+
+import java.util.List;
+
+import org.moera.commons.util.LogUtil;
+import org.moera.node.auth.Scope;
+import org.moera.node.auth.principal.Principal;
+import org.moera.node.model.GrantInfo;
+import org.springframework.data.util.Pair;
+
+public class GrantUpdatedEvent extends Event {
+
+    private GrantInfo grant;
+
+    public GrantUpdatedEvent(GrantInfo grant) {
+        super(EventType.GRANT_UPDATED, Scope.OTHER, Principal.ADMIN);
+        this.grant = grant;
+    }
+
+    public GrantInfo getGrant() {
+        return grant;
+    }
+
+    public void setGrant(GrantInfo grant) {
+        this.grant = grant;
+    }
+
+    @Override
+    public void logParameters(List<Pair<String, String>> parameters) {
+        super.logParameters(parameters);
+        parameters.add(Pair.of("nodeName", LogUtil.format(grant.getNodeName())));
+        parameters.add(Pair.of("scope", LogUtil.format(String.join(",", grant.getScope()))));
+    }
+
+}
