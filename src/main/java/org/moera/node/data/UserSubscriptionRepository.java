52049df5fc55941228ef0f950d121b0847f57115
Shmuel Melamud <smelamud@redhat.com>
2025-06-23 06:50:20 +0300

    Formatting.

--- src/main/java/org/moera/node/data/UserSubscriptionRepository.java
+++ src/main/java/org/moera/node/data/UserSubscriptionRepository.java
@@ -12,39 +12,57 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 public interface UserSubscriptionRepository extends JpaRepository<UserSubscription, UUID>,
         QuerydslPredicateExecutor<UserSubscription> {
 
-    @Query("select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
-            + " where s.nodeId = ?1 and s.id = ?2")
+    @Query(
+        "select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
+        + " where s.nodeId = ?1 and s.id = ?2"
+    )
     Optional<UserSubscription> findAllByNodeIdAndId(UUID nodeId, UUID id);
 
-    @Query("select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
-            + " where s.nodeId = ?1 and s.subscriptionType = ?2")
+    @Query(
+        "select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
+        + " where s.nodeId = ?1 and s.subscriptionType = ?2"
+    )
     List<UserSubscription> findAllByType(UUID nodeId, SubscriptionType subscriptionType);
 
-    @Query("select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
-            + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteEntryId = ?4")
+    @Query(
+        "select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
+        + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteEntryId = ?4"
+    )
     List<UserSubscription> findAllByTypeAndNodeAndEntryId(
-            UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName, String remoteEntryId);
+        UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName, String remoteEntryId
+    );
 
-    @Query("select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
-            + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteFeedName = ?4")
+    @Query(
+        "select s from UserSubscription s left join fetch s.contact c left join fetch c.remoteAvatarMediaFile"
+        + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteFeedName = ?4"
+    )
     List<UserSubscription> findAllByTypeAndNodeAndFeedName(
-            UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName, String remoteFeedName);
+        UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName, String remoteFeedName
+    );
 
     @Query("select count(*) from UserSubscription s where s.nodeId = ?1 and s.subscriptionType = ?2")
     int countByType(UUID nodeId, SubscriptionType subscriptionType);
 
-    @Query("select count(*) from UserSubscription s where s.nodeId = ?1 and s.subscriptionType = ?2"
-            + " and s.remoteNodeName = ?3")
+    @Query(
+        "select count(*) from UserSubscription s where s.nodeId = ?1 and s.subscriptionType = ?2"
+        + " and s.remoteNodeName = ?3"
+    )
     int countByTypeAndRemoteNode(UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName);
 
-    @Query("select count(*) from UserSubscription s"
-            + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteEntryId = ?4")
-    int countByTypeAndNodeAndEntryId(UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName,
-                                     String remoteEntryId);
+    @Query(
+        "select count(*) from UserSubscription s"
+        + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteEntryId = ?4"
+    )
+    int countByTypeAndNodeAndEntryId(
+        UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName, String remoteEntryId
+    );
 
-    @Query("select count(*) from UserSubscription s"
-            + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteFeedName = ?4")
-    int countByTypeAndNodeAndFeedName(UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName,
-                                      String remoteFeedName);
+    @Query(
+        "select count(*) from UserSubscription s"
+        + " where s.nodeId = ?1 and s.subscriptionType = ?2 and s.remoteNodeName = ?3 and s.remoteFeedName = ?4"
+    )
+    int countByTypeAndNodeAndFeedName(
+        UUID nodeId, SubscriptionType subscriptionType, String remoteNodeName, String remoteFeedName
+    );
 
 }
