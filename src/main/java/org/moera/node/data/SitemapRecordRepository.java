28c4d2553c1f2096b9ee591a8c273c3841d8ec11
Shmuel Melamud <smelamud@redhat.com>
2025-12-02 01:55:12 +0200

    Formatting.

--- src/main/java/org/moera/node/data/SitemapRecordRepository.java
+++ src/main/java/org/moera/node/data/SitemapRecordRepository.java
@@ -8,17 +8,21 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface SitemapRecordRepository extends JpaRepository<SitemapRecord, UUID> {
 
-    @Query("select p from Posting p"
-            + " where p.nodeId = ?1"
-            + " and exists(select s from Story s where s.entry.id = p.id and s.feedName = 'timeline')"
-            + " and not exists(select r from SitemapRecord r where r.entry.id = p.id and r.modifiedAt >= p.editedAt)")
+    @Query(
+        "select p from Posting p"
+        + " where p.nodeId = ?1"
+        + " and exists(select s from Story s where s.entry.id = p.id and s.feedName = 'timeline')"
+        + " and not exists(select r from SitemapRecord r where r.entry.id = p.id and r.modifiedAt >= p.editedAt)"
+    )
     Collection<Posting> findUpdated(UUID nodeId);
 
     @Query("select r from SitemapRecord r where r.nodeId = ?1 and r.entry.id = ?2")
     SitemapRecord findByEntryId(UUID nodeId, UUID entryId);
 
-    @Query("select new org.moera.node.data.Sitemap(r.sitemapId, count(*), max(r.modifiedAt)) from SitemapRecord r"
-            + " where r.nodeId = ?1 group by r.sitemapId")
+    @Query(
+        "select new org.moera.node.data.Sitemap(r.sitemapId, count(*), max(r.modifiedAt)) from SitemapRecord r"
+        + " where r.nodeId = ?1 group by r.sitemapId"
+    )
     Collection<Sitemap> findSitemaps(UUID nodeId);
 
     @Query("select r from SitemapRecord r where r.nodeId = ?1 and r.sitemapId = ?2 and r.visible = true")
