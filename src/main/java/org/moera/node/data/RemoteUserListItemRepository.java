1da15e2d4d6bc0347a3633c85fafbcc101e3c6a0
Shmuel Melamud <smelamud@redhat.com>
2026-04-06 05:25:03 +0300

    Formatting.

--- src/main/java/org/moera/node/data/RemoteUserListItemRepository.java
+++ src/main/java/org/moera/node/data/RemoteUserListItemRepository.java
@@ -13,22 +13,32 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RemoteUserListItemRepository extends JpaRepository<RemoteUserListItem, UUID> {
 
-    @Query("select i from RemoteUserListItem i"
-            + " where i.nodeId = ?1 and i.listNodeName = ?2 and i.listName = ?3 and i.absent = false")
+    @Query(
+        "select i from RemoteUserListItem i"
+        + " where i.nodeId = ?1 and i.listNodeName = ?2 and i.listName = ?3 and i.absent = false"
+    )
     Page<RemoteUserListItem> findNotAbsentByList(UUID nodeId, String listNodeName, String listName, Pageable pageable);
 
-    @Query("select i from RemoteUserListItem i"
-            + " where i.nodeId = ?1 and i.listNodeName = ?2 and i.listName = ?3 and i.nodeName = ?4")
+    @Query(
+        "select i from RemoteUserListItem i"
+        + " where i.nodeId = ?1 and i.listNodeName = ?2 and i.listName = ?3 and i.nodeName = ?4"
+    )
     Optional<RemoteUserListItem> findByListAndNodeName(
-            UUID nodeId, String listNodeName, String listName, String nodeName);
+        UUID nodeId, String listNodeName, String listName, String nodeName
+    );
 
-    @Query("select i from RemoteUserListItem i"
-            + " where i.nodeId = ?1 and i.listNodeName = ?2 and i.listName = ?3 and i.nodeName in (?4)")
+    @Query(
+        "select i from RemoteUserListItem i"
+        + " where i.nodeId = ?1 and i.listNodeName = ?2 and i.listName = ?3 and i.nodeName in (?4)"
+    )
     Collection<RemoteUserListItem> findByListAndNodeNames(
-            UUID nodeId, String listNodeName, String listName, Collection<String> nodeNames);
+        UUID nodeId, String listNodeName, String listName, Collection<String> nodeNames
+    );
 
-    @Query("select i from RemoteUserListItem i"
-            + " where i.nodeId = ?1 and i.listName = ?2 and i.nodeName = ?3 and i.absent = false")
+    @Query(
+        "select i from RemoteUserListItem i"
+        + " where i.nodeId = ?1 and i.listName = ?2 and i.nodeName = ?3 and i.absent = false"
+    )
     Collection<RemoteUserListItem> findByNodeNameNotAbsent(UUID nodeId, String listName, String nodeName);
 
     @Query("delete from RemoteUserListItem i where i.absent = true and i.deadline < ?1")
